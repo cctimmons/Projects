5d0714531669ea984c783062dbf10ba799d19149
cctimmons <cct27@protonmail.com>
2022-04-13 13:28:47 -0700

    Ádding practice.py file

---
+++ Practice.py
@@ -0,0 +1,21 @@
+print ('Mad Lib, Beach Fun!')
+
+noun = input('enter a noun:')
+noun1 = input ('enter a noun:')
+verb = input('enter a verb:')
+verb1 = input('enter a verb:')
+adjective = input('enter an adjective:')
+verb2 = input('enter a verb:')
+verb3 = input ('enter a verb:')
+
+
+
+
+print ('When the ' + noun + ' reaches ' + noun1 )
+print ('It is time to ' + verb + ' to the beach!' ) 
+print ('The sun is ' + verb1 +'ing' ' and the water is ' + adjective)
+print ('temperature for ' + verb2 +'ing. ' + 'There is so much ' + verb3 + ' to  do at the beach.')
+
+
+
+
